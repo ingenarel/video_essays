3f3eb4f0aa2c95fb11f3934d1f71ba8cc74c12c6
ingenarel <ingenarelitems@gmail.com>
2025-11-11 02:54:25 +0600

    (8): render, create thumbnail

---
+++ neovim/8/dir-structures/file-based-modular/config/nvim/lua/configs/keybinds/init.lua
@@ -0,0 +1,5 @@
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>")
+vim.g.mapleader = " "
+vim.keymap.set({ "n", "v" }, "<leader>wxx", "<C-w>v<C-w>w")
+vim.keymap.set({ "n", "v" }, "<leader>wz", "<C-w>|<C-w>_")
+vim.keymap.set({ "n", "v" }, "<leader>wyy", "<C-w>s<C-w>w")
